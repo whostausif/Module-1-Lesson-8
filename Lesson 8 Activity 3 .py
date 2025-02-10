c9befc525a146734a0f7bf41cee6960fcb053708
whostausif <tausifwali@gmail.com>
2025-02-10 19:57:16 +0600

    Lesson 8 Activity 3

---
+++ Lesson 8 Activity 3 .py
@@ -0,0 +1,12 @@
+mean1 = 38
+total_numbers = 40
+wrong_number = 36
+correct_number = 56
+
+sum = mean1*total_numbers
+print(sum)
+new_sum = sum-(wrong_number-correct_number)
+print(new_sum)
+
+new_mean = new_sum/total_numbers
+print(new_mean)

d8863ceab47fc50da7a959812c8bfe2cfafbf0af
whostausif <tausifwali@gmail.com>
2025-02-10 19:31:13 +0600

    Lesson 8 Activity 1

---
+++ Lesson 8 Activity 1.py
@@ -0,0 +1,23 @@
+a = 7
+z = 8
+x = 3
+y = 2
+g = 0
+
+g = (a+z)-x*y
+print("The result is",g)
+
+
+
+
+
+
+name = "Alex"
+age = 14
+
+
+if name == "Alex" or name == "John" and age >= 2:
+    print("Hello Welcome")
+
+else:
+    print("Goodbye")
